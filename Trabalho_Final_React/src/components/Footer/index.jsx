fe63d602a2b85e817de64fb5c8e6e9a439a3573d
Lorrane <lorranealocruz93@gmail.com>
2025-11-10 17:43:10 -0300

    ajeitando o footer

--- Trabalho_Final_React/src/components/Footer/index.jsx
+++ Trabalho_Final_React/src/components/Footer/index.jsx
@@ -1,7 +1,10 @@
+import { useLocation } from "react-router";
 import { Container, Text } from "./style";
 
 export function Footer() {
-    
+
+    const location = useLocation() 
+    if (location.pathname === "/login" || location.pathname === "/") return null
     return (
         <Container >
             <Text>© {new Date().getFullYear()} Loja Pokémon — Todos os direitos reservados</Text>
